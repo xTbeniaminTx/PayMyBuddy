30dc4c1fc9946d74397990ac8ee63a5f2e1b9c2c
Tolan Beniamin <39988307+xTbeniaminTx@users.noreply.github.com>
2021-06-06 13:13:11 +0200

    security configuration

--- src/main/java/fr/tolan/paymybuddy/security/SecurityConfiguration.java
+++ src/main/java/fr/tolan/paymybuddy/security/SecurityConfiguration.java
@@ -1,5 +1,37 @@
 package fr.tolan.paymybuddy.security;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/", "/register", "/register_action", "/login", "/login_action", "/webjars/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout();
+  }
 
 }
